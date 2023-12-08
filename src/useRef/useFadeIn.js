7299be917666d2a4698c7ca833f0a113388541e7
working-zima <working.zima@gmail.com>
2023-12-08 15:41:51 +0900

    refactor: ease-in-out 추가

--- src/useRef/useFadeIn.js
+++ src/useRef/useFadeIn.js
@@ -6,9 +6,9 @@ export const useFadeIn = (duration = 1, delay = 0) => {
     if (typeof duration !== "number" || typeof delay !== "number") return;
     if (element.current) {
       const { current } = element;
-      current.style.transition = `opacity ${duration}s`;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}`;
       current.style.opacity = 1;
     }
-  }, []);
+  }, [duration, delay]);
   return { ref: element, style: { opacity: 0 } };
 };
