149c60345a8d2f727656f7a8d1eb684eed745f54
working-zima <working.zima@gmail.com>
2023-12-08 15:35:32 +0900

    feat: useFadeIn

---
+++ src/useRef/useFadeIn.js
@@ -0,0 +1,14 @@
+import { useRef, useEffect } from "react";
+
+export const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof duration !== "number" || typeof delay !== "number") return;
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
+};
